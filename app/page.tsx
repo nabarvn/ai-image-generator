7145e18232755172a74feaf87400993bf4eae184
Nabarun <nicknabarun@gmail.com>
2023-05-02 21:15:49 +0530

    Fix styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Images } from "./components";
 
 export default function Home() {
   return (
-    <main className='mx-2 md:mx-0'>
+    <main className='mx-2 md:mx-0 mb-9'>
       <Images />
     </main>
   );
