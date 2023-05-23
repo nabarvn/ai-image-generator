3ba704ccda1cfc8dcc2ce62e5ab6ad609cb6fe40
Nabarun <nicknabarun@gmail.com>
2023-05-23 21:43:42 +0530

    Add `OptimizedImage` component

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Header } from "./Header";
 export { default as Prompt } from "./Prompt";
 export { default as Images } from "./Images";
+export { default as OptimizedImage } from "./OptimizedImage";
 export { default as ClientProvider } from "./ClientProvider";
