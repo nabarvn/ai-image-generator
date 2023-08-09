e14c8b463e16a2d59951237287fbee074c3f0797
Nabarun <nicknabarun@gmail.com>
2023-08-09 16:07:55 +0530

    Add image and device sizes array

--- next.config.js
+++ next.config.js
@@ -14,6 +14,11 @@ const nextConfig = {
       "links.papareact.com",
       "aigeneratedvisuals.blob.core.windows.net",
     ],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [
+      16, 32, 48, 64, 96, 128, 256, 384, 512, 640, 750, 828, 1080, 1200, 1920,
+      2048, 3840,
+    ],
   },
   experimental: {
     appDir: true,
